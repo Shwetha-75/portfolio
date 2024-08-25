7431c5056249865e2d2128177187c362b46ec467
Shwetha K <shweetha.sweety17@gmail.com>
2024-08-25 21:15:06 +0530

    Commiting the chnages in the navigation bar onhovering

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -2,73 +2,56 @@ import React from 'react'
 import "./card.css";
 import { SlArrowDown } from "react-icons/sl";
 export default function Card(props) {
+  const [onHoverMail,setOnHoverMail]=React.useState(false);
+  const [onHoverLinkedIn,setOnHoverLinkedIn]=React.useState(false);
+  const [onHoverLeetCode,setOnHoverLeetCode]=React.useState(false);
+  const [onHoverHackerRank,setOnHoverHackerRank]=React.useState(false);
 
+  
   return (
-    <div  
-   >
+    <div >
 
-      <nav id={props.status?"myNav":"hidden"}
+      <nav 
+      id={props.status?"myNav":"hidden"}
            className="div-tag-navigation-bar">
         <ul>
          
-        <li
-        
-        style={{
-          color:'white',
-          
-        }}
-        >
+        <li 
+        className={onHoverLinkedIn?'linkedIn-tag':''}
+        onMouseOver={()=>setOnHoverLinkedIn(true)} 
+        onMouseLeave={()=>setOnHoverLinkedIn(false)}>
             Linked In
           </li>
-          <li
-        
-          style={{
-            color:'white',
-         
-
-          
-          }}
-          >
-           <div className='google-tag-div--g' style={{display:'inline-flex'}}>G</div> 
-           <div className='google-tag-div--o-1' style={{display:'inline-flex'}}>o</div> 
-           <div className='google-tag-div--o' style={{display:'inline-flex'}}>o</div> 
-           <div className='google-tag-div--g' style={{display:'inline-flex'}}>g</div> 
-           <div className='google-tag-div--l' style={{display:'inline-flex'}}>l</div>   
-           <div className='google-tag-div--e' style={{display:'inline-flex'}}>e</div>   
-           <div className='google-tag-div---' style={{display:'inline-flex'}}>-</div> 
-           <div className='google-tag-div--mail' style={{display:'inline-flex'}}>{" "}mail</div> 
+          <li>
+           <div onMouseOver={()=>setOnHoverMail(true)} onMouseLeave={()=>setOnHoverMail(false)} className={onHoverMail?'google-tag-div--g':""} style={{display:'inline-flex'}}>G</div> 
+           <div onMouseOver={()=>setOnHoverMail(true)} onMouseLeave={()=>setOnHoverMail(false)} className={onHoverMail?'google-tag-div--o-1':""} style={{display:'inline-flex'}}>o</div> 
+           <div onMouseOver={()=>setOnHoverMail(true)} onMouseLeave={()=>setOnHoverMail(false)} className={onHoverMail?'google-tag-div--o':""} style={{display:'inline-flex'}}>o</div> 
+           <div onMouseOver={()=>setOnHoverMail(true)} onMouseLeave={()=>setOnHoverMail(false)} className={onHoverMail?'google-tag-div--g':""} style={{display:'inline-flex'}}>g</div> 
+           <div onMouseOver={()=>setOnHoverMail(true)} onMouseLeave={()=>setOnHoverMail(false)} className={onHoverMail?'google-tag-div--l':""} style={{display:'inline-flex'}}>l</div>   
+           <div onMouseOver={()=>setOnHoverMail(true)} onMouseLeave={()=>setOnHoverMail(false)} className={onHoverMail?'google-tag-div--e':""} style={{display:'inline-flex'}}>e</div>   
+           <div onMouseOver={()=>setOnHoverMail(true)} onMouseLeave={()=>setOnHoverMail(false)} className={onHoverMail?'google-tag-div---':""} style={{display:'inline-flex'}}>-</div> 
+           <div onMouseOver={()=>setOnHoverMail(true)} onMouseLeave={()=>setOnHoverMail(false)} className={onHoverMail?'google-tag-div--mail':''} style={{display:'inline-flex'}}>{" "}mail</div> 
 
           </li>
           <li
-          
-          style={{
-            color:'white',
-            
-          }}
+          className={onHoverLeetCode?'leetcode-tag':''}
+          onMouseOver={()=>setOnHoverLeetCode(true)}
+          onMouseLeave={()=>setOnHoverLeetCode(false)}
           >
            Leet Code
           </li>
           <li
-          
-          style={{
-            color:'white',
-            
-          }}
-          >
+          className={onHoverHackerRank?'hackerRank-tag':''}
+          onMouseOver={()=>setOnHoverHackerRank(true)}
+          onMouseLeave={()=>setOnHoverHackerRank(false)} >
            Hacker Rank
           </li>
 
         </ul>
-        <div
-        style={{
-          width:'100%',
-        
-        }}
-           onClick={props.handleOnClickStatus}
-        >
+        <div onClick={props.handleOnClickStatus}>
         
           
-     </div>
+       </div>
         
         </nav>   
     
