0b7942bb7fd4e62f894c0610afd4cab5eb385c94
Shwetha K <shweetha.sweety17@gmail.com>
2024-11-09 21:40:45 +0530

    Styling on forms

--- src/Component/Form/Main.jsx
+++ src/Component/Form/Main.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React from 'react';
+import "./form.css";
 import {Form} from 'react-bootstrap';
+
 export default function Main() {
   return (
-    <div>
+    <>
 
-<Form>
+<Form className='container mt-3'>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Label className='input--tag--label'>Email address</Form.Label>
+        <Form.Control type="email--tag--type" placeholder="name@example.com" className='form--tag--input'/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label className='input--tag--label'>Email address</Form.Label>
+        <Form.Control  placeholder="name@example.com" className='form--tag--input'/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        <Form.Label className='input--tag--label'>Example textarea</Form.Label>
+        <Form.Control as="textarea" rows={3} className='form--tag--input'/>
       </Form.Group>
-      <button>Submitz</button>
+      <button>Submit</button>
     </Form>
-    </div>
+  
+    </>
   )
 }
