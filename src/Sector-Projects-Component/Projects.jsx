409851dc44f0b03b9e4f9e2cfc4347994f5249dc
Shwetha K <shweetha.sweety17@gmail.com>
2024-12-21 18:35:22 +0530

    24-12-21_Portfolio_[Shwetha K] : Update on Card slider still facing issue in styling

--- src/Sector-Projects-Component/Projects.jsx
+++ src/Sector-Projects-Component/Projects.jsx
@@ -1,33 +1,54 @@
 import React,{forwardRef} from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Data from "./data";
+import Slider from "react-slick";
 import "./project.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 export default forwardRef(
-    function Projects(props,ref) {
-      // const windowWidth = React.useRef(window.innerWidth);
-      // const windowHeight = React.useRef(window.innerHeight);
-    
-      // console.log('width: ', windowWidth.current);
-      // console.log('height: ', windowHeight.current);
-      // console.log("Hey I'm Projects executing here ")
-
-        return (
-          <div 
-          className='section--tag'
-          ref={ref}>
-          
-           <div className="container-fluid">
-
-  <div className="row">
-  <div className="col-sm-4" id='column--tag'>.col-sm-4</div>
-  <div className="col-sm-4" id='column--tag--two'>.col-sm-4</div>
-  <div className="col-sm-4" id='column--tag--three'>.col-sm-4</div>
-</div>
-</div>
-          
   
 
+    function Projects(props,ref) {
+      
+      const settings = {
+        dots: true,
+        lazyLoad: true,
+        focusOnSelect: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed:1500,
+        autoplaySpeed: 1500,
+        cssEase: "linear",
+        
+      };
+
+      const array=Data.map((item)=>(
+        <div key={item.id} id=" "  className='bg-black h-[450px] text-white rounded-xl border border-sky-500'>
+          <div className='h-56 rounded-t-xl bg-black-500 flex justify-center items-center'>
+            <img  src={item.image} alt='' className="h-44 w-44 rounded-full"/>
           </div>
+          <div className='flex flex-col justify-center items-center g-4 p-4'>
+            <p className='text-xl text-sky-500 font-semibold p-3'>{item.name}</p>
+            <p>{item.para}</p>
+            <button className='bg-sky-900 text-white text-lg mt-10 px-6 py-3 rounded-xl'>Read more</button>
+          </div>
+        </div>
+      ))
+        return (
+          <div className='w-4/5 m-auto' ref={ref}>
+         
+          <div className='mt-20' >
+      <Slider {...settings}>
+    {array}
+    </Slider>
+    </div>
+    </div>
+         
+        
         )
       }
+      
 )
 
---
+++ src/Sector-Projects-Component/data.js
@@ -0,0 +1,29 @@
+import Image1 from "./media/image1.png";
+import Image2 from "./media/image2.png";
+var data=[
+    {
+        id:1,
+        image:Image2,
+        name:"text_1",
+        para:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+
+    },
+    {
+        id:2,
+        image:Image1,
+        name:"text_2",
+        para:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+
+    },
+
+    {
+        id:3,
+        image:Image2,
+        name:"text_3",
+        para:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+
+    },
+    
+];
+
+export default data;
