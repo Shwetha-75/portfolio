602d2f36edbe8a7259ec36aca82c38c5e8e6c810
Shwetha K <shweetha.sweety17@gmail.com>
2025-01-04 16:55:16 +0530

    25-01-04_Portfolio_[Shwetha]_:_Home-freezed, About-Freezed,Contact-Freezed

--- src/Sector-Home-Component/Home.jsx
+++ src/Sector-Home-Component/Home.jsx
@@ -1,9 +1,41 @@
 import React,{forwardRef} from 'react'
 import "./home.css";
 import video from "./video1.mp4";
-export default forwardRef(
+import Slider from "react-slick";
 
+export default forwardRef(
+     
     function Home(props,ref) {    
+      const array=[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1].map((item,index)=>(
+       
+          <p key={index} className='text-green-600'>{item}</p>
+        
+      ));
+        
+
+      const settings = {
+        dots: true, 
+        infinite: true, 
+        autoplay: true,
+        speed: 300, 
+        autoplaySpeed: 300,
+        slidesToShow: 10, 
+        slidesToScroll: 10,
+      
+       
+        
+      };
+
+      let result=[];
+      for(let i=0;i<7;i++){
+        result.push(
+          <Slider key={i} className='overflow-y-hidden slider--tag--binary' {...settings}>
+          {array}
+
+         </Slider>
+        )
+      }
+
         return (
           <div 
           className='section--tag'
@@ -13,18 +45,29 @@ export default forwardRef(
             <div className='list--tag--home--page'>
               <ul>
                 <li>Shwetha</li>
-                <li>Engineering Graduate</li>
+                <li id='title--tag'>
+                  Computer Science & Engineering Graduate</li>
               </ul>
-              <div className='box--tag'>
-
+              <div className='box--tag overflow-y-hidden'>
+                {result}
+            
+               
+                
+              </div>
+              <div className='present--title--tag'>
+                <p className='present--tag'>Present,</p>
+                <p>Automation Engineer - GAT{' '}
+                  <span>@ Volvo Group India Pvt Ltd.</span> 
+                  </p>
               </div>
             </div>
 
             <div className='line'>
+
             </div>
 
               
-            <div className='video--tag' >
+            <div className='video--tag'>
             <video autoPlay muted loop fluid='false' width='200px'>
               <source src={video} type="video/mp4" >
               </source>
--- src/Sector-Projects-Component/Projects.jsx
+++ src/Sector-Projects-Component/Projects.jsx
@@ -1,49 +1,84 @@
-import React,{forwardRef} from 'react'
+import React, { useState, forwardRef } from "react";
 import data from "./data.js";
 import "./project.css";
 import Slider from "react-slick";
-export default forwardRef(
-  
 
-    function Projects(props,ref) {
-      const settings = {
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        speed: 4000,
-        autoplaySpeed: 4000,
-        cssEase: "linear"
-      };
-    
-         const array=data.map((item)=>(
-          <div key={item.id} id="card--tag"  className='bg-black  h-50 text-white rounded-xl ' >
-          <div className='h-26 rounded-t-xl bg-black-500 flex justify-center text-justify items-center'>
-            <img  src={item.image} alt='' className="h-24 w-24 rounded-full"/>
-          </div>
-          <div className='flex flex-col text-center justify-center items-center g-4 p-4'>
-            <p className='text-xl text-lime-500 font-semibold p-3'>{item.name}</p>
-            <p>{item.para}</p>
-            <button className='border border-lime-500 text-white w-40 text-lg mt-10 px-3 py-2 rounded-xl btn--project'>Read more</button>
-          </div>
-        </div>
-         ))
-
-        return (
-          <div className='section--tag m-auto ' id='section--tag--project' ref={ref}>
-          <div className='w-3/4 m-auto'>
-          <div className='mt-10' id="project--tag" style={{display:'flex'}}>
-          <Slider className='overflow-y-hidden' {...settings}>
-            {array}
-            </Slider>
-           </div>
-           </div> 
-           </div>
-         
+const Popup = ({ item, onClose }) => {
+  return (
+    <div className="popup-overlay">
+      <div className="popup-content bg-black text-white rounded-xl p-7">
         
-        )
-      }
+        <div className="text-center">
+          <img
+            src={item.image}
+            alt={item.name}
+            className="h-24 w-24 rounded-full mx-auto mb-4"
+          />
+          <h2 className="text-xl text-lime-500 font-semibold">{item.name}</h2>
+          <p className="mt-4">{item.para}</p>
+          <button
+          className="popup-close mt-10 text-white px-3 py-1 rounded-full"
+          onClick={onClose} >
+          Close
+        </button>
+        </div>
+       
+      </div>
       
-)
+    </div>
+  );
+};
 
+export default forwardRef(function Projects(props, ref) {
+  const [popupItem, setPopupItem] = useState(null);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 4000,
+    autoplaySpeed: 4000,
+    cssEase: "linear",
+  };
+
+  const handleReadMore = (item) => {
+    setPopupItem(item);
+  };
+
+  const closePopup = () => {
+    setPopupItem(null);
+  };
+
+  const cards = data.map((item) => (
+    <div key={item.id} id="card--tag" className="bg-black h-50 text-white rounded-xl">
+      <div className="h-26 rounded-t-xl bg-black-500 flex justify-center text-justify items-center">
+        <img src={item.image} alt="" className="h-24 w-24 rounded-full" />
+      </div>
+      <div className="flex flex-col text-center justify-center items-center g-4 p-4">
+        <p className="text-xl text-lime-500 font-semibold p-3">{item.name}</p>
+        <p>{item.para}</p>
+        <button
+          className="border border-lime-500 text-white w-40 text-lg mt-10 px-3 py-2 rounded-xl btn--project"
+          onClick={() => handleReadMore(item)}
+        >
+          Read more
+        </button>
+      </div>
+    </div>
+  ));
+
+  return (
+    <div className="section--tag m-auto" id="section--tag--project" ref={ref}>
+      <div className="w-3/4 m-auto">
+        <div className="mt-10" id="project--tag" style={{ display: "flex" }}>
+          <Slider className="overflow-y-hidden" {...settings}>
+            {cards}
+          </Slider>
+        </div>
+      </div>
+      {popupItem && <Popup item={popupItem} onClose={closePopup} />}
+    </div>
+  );
+});
