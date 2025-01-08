8b05ac31f4d0ba0cae9e0029ecca7eceb82126bb
Shwetha K <105779009+Shwetha-75@users.noreply.github.com>
2025-01-08 18:03:53 +0530

    Update social_media.js

--- src/Sector-Footer-Component/social_media.js
+++ src/Sector-Footer-Component/social_media.js
@@ -1,10 +1,10 @@
  var social_media={
-    linkedIn:'https://www.linkedin.com/in/shwetha-k-/',
-    mail:'shwethak412@gmail.com',
-    daily_dev:'https://app.daily.dev/shwethak',
-    dev:'https://dev.to/shwetha75',
-    leetCode:'https://leetcode.com/u/SHWETHA_K/',
-    hackerRank:'https://www.hackerrank.com/profile/shwetha_675',
-    git:'https://github.com/Shwetha-75'
+    linkedIn:'',
+    mail:'',
+    daily_dev:'',
+    dev:'',
+    leetCode:'',
+    hackerRank:'',
+    git:''
 }
 export default social_media;
